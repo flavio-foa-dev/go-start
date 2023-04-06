b235169096d44f78bad37bfd897869e1ec6da9e5
Flavio Andrade <foa12381@gmail.com>
2023-04-06 08:49:32 -0300

    first commit func/init

---
+++ src/hello.go
@@ -0,0 +1,19 @@
+// Declaration of the main package
+package main
+
+// Importing package
+import "fmt"
+
+// Multiple init() function
+func init() {
+	fmt.Println("Welcome to init() function")
+}
+
+func init() {
+	fmt.Println("Hello! init() function")
+}
+
+// Main function
+func main() {
+	fmt.Println("Welcome to main() function")
+}
